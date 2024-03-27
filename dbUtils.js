a5fe1f82e46d0fd1e21cb49afda5a1d3974a7906
ruchitha <ruchithams1@gmail.com>
2024-03-27 14:52:35 +0530

    fixing get tickets query

--- dbUtils.js
+++ dbUtils.js
@@ -105,7 +105,7 @@ async function getAllTickets() {
     try {
         const { data, error } = await supabase
             .from('tickets')
-            .select('*');
+            .select('username', 'email', 'description', 'response', 'createdat', 'updatedat','status');
 
         return data;
     } catch (error) {
